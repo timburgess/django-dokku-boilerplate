0c9479c6aba127a4068d11d5184ac8c4cf333fa5
Tim Burgess <tim.burgess@pacificedge.co.nz>
2020-07-23 13:12:44 +1200

    Drop internal secret key

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,9 +38,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '761j#@zu7=54(&g*_6k8fp^_(=z(utuxh3-4d8@)n4%y9&2fiq'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
