c4945266c1395339760e748b9293b3b937825f64
Tim Burgess <tim.burgess@pacificedge.co.nz>
2020-07-23 12:44:08 +1200

    Build out structure for dokku

---
+++ config/settings/local.py
@@ -0,0 +1,25 @@
+from .base import *  # noqa
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="Y64mmoiwVaNqzyMQRcNGYxzicHCOGx9QQERsODNsbWqZSEb1iFzG1kkPKFlP3eGE",
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+# CACHES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#caches
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+#         "LOCATION": "",
+#     }
+# }
+
