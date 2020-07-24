f2f79faf85501584a18e182a2546086c99cdab35
Tim Burgess <tim.burgess@pacificedge.co.nz>
2020-07-24 12:01:59 +1200

    Add a default secret key

--- config/settings/local.py
+++ config/settings/local.py
@@ -6,6 +6,12 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
 
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="Y64mmoiwVaNqzyMQRcNGYxzicHCOGx9QQERsODNsbWqZSEb1iFzG1kkPKFlP3eGE",
+)
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
 
